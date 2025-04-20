f0343e270ac7d94288416fc1b08ab2bc1ead9312
ms <msjung9402@naver.com>
2025-04-20 20:23:35 +0900

    server 수정

--- server.js
+++ server.js
@@ -2,13 +2,7 @@
 
 require('dotenv').config();
 
-const express = require('express');
-const cors = require('cors');
-
-const app = express();
-
-app.use(cors());
-app.use(express.json());
+const app = require('./app');
 
 const PORT = process.env.PORT || 3000;
 
